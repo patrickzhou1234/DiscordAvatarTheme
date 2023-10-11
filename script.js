b59ca354e662f757485ca5c0197353ea2d036578
Patrick Zhou <patrickchang44543@gmail.com>
2023-10-10 17:42:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,8 @@
 // @grant        none
 // ==/UserScript==
 
-let injectHTML = '<svg width="96" height="96" viewBox="0 0 96 96" class="avatarDecoration-2Wb1Au" aria-hidden="true"><foreignObject x="0" y="0" width="96" height="96" mask="url(#)"><div class="avatarStack-3Bjmsl"><img class="avatar-31d8He" src="https://cdn.discordapp.com/avatar-decoration-presets/a_c7e1751e8122f1b475cb3006966fb28c.png?size=128&amp;passthrough=true" alt=" " aria-hidden="true"></div></foreignObject></svg>';
-let profInject = '<svg width="140" height="140" viewBox="0 0 140 140" class="avatarDecoration-2Wb1Au" aria-hidden="true"><foreignObject x="0" y="0" width="140" height="140" mask="url(#)"><div class="avatarStack-3Bjmsl"><img class="avatar-31d8He" src="https://cdn.discordapp.com/avatar-decoration-presets/a_c7e1751e8122f1b475cb3006966fb28c.png?size=240&amp;passthrough=true" alt=" " aria-hidden="true"></div></foreignObject></svg>';
+let injectHTML = '<svg width="108" height="96" viewBox="0 0 108 96" class="avatarDecoration-2Wb1Au" aria-hidden="true"><foreignObject x="0" y="0" width="96" height="96" mask="url(#svg-mask-avatar-decoration-status-round-80)"><div class="avatarStack-3Bjmsl"><img class="avatar-31d8He" src="https://cdn.discordapp.com/avatar-decoration-presets/a_c7e1751e8122f1b475cb3006966fb28c.png?size=128&amp;passthrough=true" alt=" " aria-hidden="true"></div></foreignObject></svg>';
+let profInject = '<svg width="162" height="144" viewBox="0 0 162 144" class="avatarDecoration-2Wb1Au" aria-hidden="true"><foreignObject x="0" y="0" width="144" height="144" mask="url(#svg-mask-avatar-decoration-status-round-120)"><div class="avatarStack-3Bjmsl"><img class="avatar-31d8He" src="https://cdn.discordapp.com/avatar-decoration-presets/a_c7e1751e8122f1b475cb3006966fb28c.png?size=240&amp;passthrough=true" alt=" " aria-hidden="true"></div></foreignObject></svg>';
 setInterval(() => {
     if (document.querySelectorAll(".clickable-GKg4Qy").length > 0 && document.querySelector(".clickable-GKg4Qy").childNodes[0].childNodes[0].childNodes.length < 2) {
         document.querySelector(".clickable-GKg4Qy").childNodes[0].childNodes[0].innerHTML += injectHTML;
